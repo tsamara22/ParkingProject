efa5999a7e2e090fcf5ad00e768ca7cd23c1f288
Tayseer Samara <tayseersamara@gmail.com>
2018-03-14 14:13:45 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,8 @@
 
 
     // Initial Values for Contact
-    var name = "";
+    var first = "";
+    var last = "";
     var email = "";
     var phone = "";
     var comment = "";
@@ -27,7 +28,12 @@
     var year = "";
     var make = "";
     var model = "";
-    //var color = "";
+    var color = "";
+
+    var dateReturn = "";
+    var airline = "";
+    var flight = "";
+    var arriving = "";
 
     // Capture Button Click
     $("#add-user").on("click", function(event) {
@@ -36,11 +42,14 @@
 
 
 
+
+
       // YOUR TASK!!!
       // Code in the logic for storing and retrieving the most recent user.
       // Don't forget to provide initial data to your Firebase database.
       //Contact
-      name = $("#name-input").val().trim();
+      first = $("#first-input").val().trim();
+      last = $("#last-input").val().trim();
       email = $("#email-input").val().trim();
       phone = $("#phone-input").val().trim();
       comment = $("#comment-input").val().trim();
@@ -50,12 +59,19 @@
       year = $("#car-years").val().trim();
       make = $("#car-makes").val().trim();
       model = $("#car-models").val().trim();
-      //color = $("#vehicle-color").val().trim();
+      color = $("#car-color").val().trim();
+
+      //Return Flight
+      dateReturn = $("#return-input").val().trim();
+      airline = $("#airline-input").val().trim();
+      flight = $("#flight-input").val().trim();
+      arriving = $("#arriving-input").val().trim();
 
       // Code for the push
       dataRef.ref().push({
 
-        name: name,
+        first: first,
+        last: last,
         email: email,
         phone: phone,
         comment: comment,
@@ -63,7 +79,11 @@
         year: year,
         make: make,
         model: model,
-        //color: color,
+        color: color,
+        dateReturn: dateReturn,
+        airline: airline,
+        flight: flight,
+        arriving: arriving,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
     });
@@ -72,8 +92,9 @@
     dataRef.ref().on("child_added", function(childSnapshot) {
 
       // Log everything that's coming out of snapshot
-      console.log(childSnapshot.val().name);
-      console.log(childSnapshot.val().name);
+ 
+      console.log(childSnapshot.val().first);
+      console.log(childSnapshot.val().last);
       console.log(childSnapshot.val().email);
       console.log(childSnapshot.val().phone);
       console.log(childSnapshot.val().comment);
@@ -81,19 +102,24 @@
       console.log(childSnapshot.val().year);
       console.log(childSnapshot.val().make);
       console.log(childSnapshot.val().model);
-      //console.log(childSnapshot.val().color);
+      console.log(childSnapshot.val().color);
+      console.log(childSnapshot.val().dateReturn);
+      console.log(childSnapshot.val().airline);
+      console.log(childSnapshot.val().flight);
+      console.log(childSnapshot.val().arriving);
 
-      console.log(childSnapshot.val().joinDate);
+
+      //console.log(childSnapshot.val().joinDate);
 
 
 
       // full list of items to the well
-      $(".member-list").append("<tr><td><input type='checkbox' name='record'></td><td>" + childSnapshot.val().name +  "</td><td>" + childSnapshot.val().email + "</td><td>" + childSnapshot.val().phone + "</td><td>" + childSnapshot.val().comment + "</td></tr>");
+      $(".member-list").append("<tr><td><a href='update-form.html'><span class='glyphicon glyphicon-edit'></span></a><input type='checkbox' hidden='hidden' class='edit' name='record'></td><td>" + childSnapshot.val().last +  "</td><td>" + childSnapshot.val().email + "</td><td>" + childSnapshot.val().phone + "</td><td>" + childSnapshot.val().comment + "</td></tr>");
       
 
 
   // Clears all of the text-boxes
-      $("#name-input").val("");
+      //$("#name-input").val("");
 
 
     // Handle the errors
@@ -104,7 +130,8 @@
     dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
 
       // Change the HTML to reflect
-      $("#name-display").text(snapshot.val().name);
+      $("#first-display").text(snapshot.val().first);
+      $("#last-display").text(snapshot.val().last);
       $("#email-display").text(snapshot.val().email);
       $("#phone-display").text(snapshot.val().phone);
       $("#comment-display").text(snapshot.val().comment);
@@ -112,9 +139,32 @@
       $("#year-display").text(snapshot.val().year);
       $("#make-display").text(snapshot.val().make);
       $("#model-display").text(snapshot.val().model);
-      //$("#color-display").text(snapshot.val().color);
+      $("#color-display").text(snapshot.val().color);
+      $("#Return-display").text(snapshot.val().dateReturn);
+      $("#airline-display").text(snapshot.val().airline);
+      $("#flight-display").text(snapshot.val().flight);
+      $("#arriving-display").text(snapshot.val().arriving);
       
     });
+
+          // Log everything that's coming out of snapshot
+
+      console.log(childSnapshot.val().first);
+      console.log(childSnapshot.val().last);
+      console.log(childSnapshot.val().email);
+      console.log(childSnapshot.val().phone);
+      console.log(childSnapshot.val().comment);
+      console.log(childSnapshot.val().plate);
+      console.log(childSnapshot.val().year);
+      console.log(childSnapshot.val().make);
+      console.log(childSnapshot.val().model);
+      console.log(childSnapshot.val().color);
+      console.log(childSnapshot.val().dateReturn);
+      console.log(childSnapshot.val().airline);
+      console.log(childSnapshot.val().flight);
+      console.log(childSnapshot.val().arriving);
+
+    
 
 
 
